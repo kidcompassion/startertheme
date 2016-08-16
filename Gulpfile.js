b5bd102bdc5c5e94d674171ca2e1efa48fca9fbe
Sally Poulsen <sallypoulsen@gmail.com>
2016-08-16 14:17:19 -0600

    Add assets

---
+++ Gulpfile.js
@@ -0,0 +1,14 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+
+gulp.task('styles', function() {
+    gulp.src('assets/scss/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./'));
+});
+
+//Watch task
+gulp.task('default',function() {
+    gulp.watch('assets/scss/*.scss',['styles']);
+});
