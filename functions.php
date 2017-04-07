ac28895adf96aba70a4da8de2c389298b1969318
kidcompassion <sallypoulsen@gmail.com>
2017-04-07 12:40:39 -0600

    Adds in menus.

--- functions.php
+++ functions.php
@@ -2,17 +2,17 @@
 
 //Enqueue scripts and styles for use
 add_action( 'wp_enqueue_scripts', 'arls_load_scripts' );
-function arls_load_scripts(){
+function startertheme_load_scripts(){
     wp_enqueue_style( 'startertheme', get_template_directory_uri() . '/style.css');
     wp_enqueue_script( 'jquery' );
 }
 
 //Register menus
-function arls_register_menus() {
+function startertheme_register_menus() {
   register_nav_menu('header-menu',__( 'Header Menu' ));
   register_nav_menu('utility-menu',__( 'Utility Menu' ));
 }
-add_action( 'init', 'arls_register_menus' );
+add_action( 'init', 'startertheme_register_menus' );
 
 
 ;?>
